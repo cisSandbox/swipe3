6080a3334be2fc61e8d7c467db01aeb4a779407e
tjshah050291 <tjshah050291@gmail.com>
2015-09-15 11:19:59 -0400

    Corrections

--- database tables and php scripts/temporary.php
+++ database tables and php scripts/temporary.php
@@ -4,7 +4,7 @@ $myFile = "/Users/tejas/Downloads/trials.dat";
 $fh = fopen($myFile, 'w') or die("Could not open: " . mysql_error());
 fwrite($fh, $string);
 fclose($fh);
-$connect = mysql_connect("localhost", "root", "password")
+$connect = mysql_connect("localhost", "root", "root")
 or die('Could not connect: ' . mysql_error());
 $bool = mysql_select_db("SwipeSystem", $connect);
 if ($bool === False){
